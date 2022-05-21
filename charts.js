e47e12abbdfd9d2fc1328c417da9ca77f080824b
Ben Salamin <bensalamin@MacBook-Pro.local>
2022-05-21 12:25:02 -0400

    Updating bubble and gauge charts.

--- charts.js
+++ charts.js
@@ -101,68 +101,68 @@ function buildMetadata(sample) {
 
   // DELIVERABLE 2
   // Bubble Chart
-      // 1. Create the trace for the bubble chart.
-      var bubbleData = {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        mode: "markers",
-        marker: {
-          size: sampleValues,
-          color: otu_ids,
-          colorscale: "Earth"
-        }
-      };
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sampleValues,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    };
 
-      // 2. Create the layout for the bubble chart.
-      var bubbleLayout = {
-        title: 'Bacteria Cultures Per Sample',
-        showlegend: false,
-        xaxis: {title: "OTU ID", automargin: true},
-        yaxis: {automargin: true},
-        hovermode: "closest"
-      };
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      showlegend: false,
+      xaxis: {title: "OTU ID", automargin: true},
+      yaxis: {automargin: true},
+      hovermode: "closest"
+    };
 
-      // 3. Use Plotly to plot the data with the layout.
-      Plotly.newPlot("bubble", [bubbleData], bubbleLayout, {responsive: true});
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", [bubbleData], bubbleLayout, {responsive: true});
 
   // DELIVERABLE 3
   // Gauge Chart
-      // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-      var metadata = data.metadata;
-      var filters = metadata.filter(data => data.id == sample);
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var filters = metadata.filter(data => data.id == sample);
 
-      // 2. Create a variable that holds the first sample in the metadata array.
-      var results = filters[0];
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var results = filters[0];
 
-      // 3. Create a variable that holds the washing frequency.
-      var washFreq = results.wfreq;
-      
-      // 4. Create the trace for the gauge chart.
-      var gaugeData = {
-        value: wfreqs,
-        type: "indicator",
-        mode: "gauge+number",
-        title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
-        gauge: {
-          axis: {range: [null,10], dtick: "2"},
-          bar: {color: "black"},
-          steps:[
-            {range: [0, 2], color: "red"},
-            {range: [2, 4], color: "orange"},
-            {range: [4, 6], color: "yellow"},
-            {range: [6, 8], color: "green"},
-            {range: [8, 10], color: "blue"}
-          ],
-        },
-      };
-      
-      // 5. Create the layout for the gauge chart.
-      var gaugeLayout = {
-        hovermode: "closest", 
-      };
+    // 3. Create a variable that holds the washing frequency.
+    var washFreq = results.wfreq;
+    
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = {
+      value: wfreqs,
+      type: "indicator",
+      mode: "gauge+number",
+      title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
+      gauge: {
+        axis: {range: [null,10], dtick: "2"},
+        bar: {color: "black"},
+        steps:[
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "green"},
+          {range: [8, 10], color: "blue"}
+        ],
+      },
+    };
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = {
+      hovermode: "closest", 
+    };
 
-      // 6. Use Plotly to plot the gauge data and layout.
-      Plotly.newPlot("gauge", [gaugeData], gaugeLayout, {responsive: true});
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", [gaugeData], gaugeLayout, {responsive: true});
   });
 }
