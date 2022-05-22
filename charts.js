5f31219f7f6a8d7fd6bc3241a46fa6b83d9758a1
Ben Salamin <bensalamin@MacBook-Pro.local>
2022-05-22 17:54:42 -0400

    update

--- charts.js
+++ charts.js
@@ -15,7 +15,7 @@ function init() {
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
-  }
+}
 
   // Initialize the dashboard
   init();
@@ -32,7 +32,8 @@ function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = metadata.filter((sampleObj) => 
+    sampleObj.id == sample);
     var result = resultArray[0];
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
@@ -58,7 +59,9 @@ function buildMetadata(sample) {
       var samples = data.samples;
 
       // 4. Create a variable that filters the samples for the object with the desired sample number.
-      var resultArray = samples.filter(data => data.id == sample);
+      var resultArray = samples.filter(
+        (sampleObj) => sampleObj.id == sample
+      );
 
       //  5. Create a variable that holds the first sample in the array.
       var firstSample = resultArray[0];
@@ -72,21 +75,22 @@ function buildMetadata(sample) {
       // Hint: Get the the top 10 otu_ids and map them in descending order  
       //  so the otu_ids with the most bacteria are last. 
 
-      var yticks = otu_ids.slice(0, 10).map(id => "OTU " + id).reverse();
-
+      var yticks = otu_ids.slice(0, 10).map(
+        (otuID) => `OTU ${otuID}`);
+  
       // 8. Create the trace for the bar chart. 
       var barData = {
         y: yticks,
         x: sample_values.slice(0, 10).reverse(),
         type: "bar",
         orientation: "h",
-        text: otu_labels,
       };
       // 9. Create the layout for the bar chart. 
       var barLayout = {
         title: "Top 10 Bacteria Cultures Found",
         hovermode: "closest",
       };
+
       // 10. Use Plotly to plot the data with the layout. 
       Plotly.newPlot("bar", [barData], barLayout);
     
@@ -108,7 +112,7 @@ function buildMetadata(sample) {
       text: otu_labels,
       mode: "markers",
       marker: {
-        size: sampleValues,
+        size: sample_values,
         color: otu_ids,
         colorscale: "Earth"
       },
@@ -124,12 +128,15 @@ function buildMetadata(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", [bubbleData], bubbleLayout, {responsive: true});
+    Plotly.newPlot("bubble", [bubbleData], 
+    bubbleLayout, {responsive: true});
 
   // DELIVERABLE 3
   // Gauge Chart
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var filters = metadata.filter(data => data.id == sample);
+    var metadata = data.metadata;
+    var filters = metadata.filter(
+      (sampleObj) => sampleObj.id == sample);
 
     // 2. Create a variable that holds the first sample in the metadata array.
     var results = filters[0];
